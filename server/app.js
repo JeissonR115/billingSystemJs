e2018a43723551e61cf4137674499877fb54086f
eragon1020 <brayan.aragon.zabala@gmail.com>
2024-10-18 21:06:27 -0500

    agregar cors

--- server/app.js
+++ server/app.js
@@ -1,11 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import routes from "./src/routes/index.js";
 import connectToDatabase from "./src/config/database.js";
 
 dotenv.config();
 
 const app = express();
+
+// Configuración de CORS
+app.use(cors());
 
 const { PORT, DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
 app.use(express.json());
--- server/sqlScripts/execute.sql
+++ server/sqlScripts/execute.sql
@@ -1,11 +1,11 @@
-SOURCE /media/disk/Dev/billingSystem/server/sqlScripts/billing_system.sql;
+SOURCE C:\Users\briam\OneDrive\Documentos\UNIVERSIDAD\modelamiento\billingSystemJs/server/sqlScripts/billing_system.sql;
 
 
-SOURCE /media/disk/Dev/billingSystem/server/sqlScripts/sp/custormers.sql;
-SOURCE /media/disk/Dev/billingSystem/server/sqlScripts/sp/product.sql;
-SOURCE /media/disk/Dev/billingSystem/server/sqlScripts/sp/invoice.sql;
-SOURCE /media/disk/Dev/billingSystem/server/sqlScripts/sp/invoice_details.sql;
-SOURCE /media/disk/Dev/billingSystem/server/sqlScripts/sp/payment.sql;
+SOURCE C:\Users\briam\OneDrive\Documentos\UNIVERSIDAD\modelamiento\billingSystemJs/server/sqlScripts/sp/custormers.sql;
+SOURCE C:\Users\briam\OneDrive\Documentos\UNIVERSIDAD\modelamiento\billingSystemJs/server/sqlScripts/sp/product.sql;
+SOURCE C:\Users\briam\OneDrive\Documentos\UNIVERSIDAD\modelamiento\billingSystemJs/server/sqlScripts/sp/invoice.sql;
+SOURCE C:\Users\briam\OneDrive\Documentos\UNIVERSIDAD\modelamiento\billingSystemJs/server/sqlScripts/sp/invoice_details.sql;
+SOURCE C:\Users\briam\OneDrive\Documentos\UNIVERSIDAD\modelamiento\billingSystemJs/server/sqlScripts/sp/payment.sql;
 
 
-SOURCE /media/disk/Dev/billingSystem/server/sqlScripts/demo.sql;
+SOURCE C:\Users\briam\OneDrive\Documentos\UNIVERSIDAD\modelamiento\billingSystemJs/server/sqlScripts/demo.sql;
